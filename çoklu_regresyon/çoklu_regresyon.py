8b88bfafd09db2b4c65a57af13bf9584bec2cd0f
mertsparlak <mertsparlak@gmail.com>
2024-07-18 01:06:43 +0300

    Update çoklu_regresyon.py

--- çoklu_regresyon/çoklu_regresyon.py
+++ çoklu_regresyon/çoklu_regresyon.py
@@ -56,7 +56,7 @@ print(sonuc2)
 cinsiyet=veriler.iloc[:,-1].values
 print(cinsiyet)
 
-sonuc3=pd.DataFrame(data=cinsiyet,index=range(22),columns=["cinsiyet"])
+sonuc3=pd.DataFrame(data=c[:,:1],index=range(22),columns=["cinsiyet"])
 print(sonuc3)
 
 
@@ -67,16 +67,52 @@ s2=pd.concat([s,sonuc3],axis=1)
 print(s2)
 
 
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(s,sonuc3,test_size=0.33,random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(x_train,y_train) #x trainden y traini öğren demek 
+
+y_pred=regressor.predict(x_test) # cinsiyeti diğer değişkenlere göre tahmin ettirdik
+
+boy=s2.iloc[:,3:4].values
+print(boy)
+
+sol=s2.iloc[:,:3]
+sag=s2.iloc[:,4:]
+
+veri=pd.concat([sol,sag],axis=1)
+
+x_train,x_test,y_train,y_test=train_test_split(veri,boy,test_size=0.33,random_state=0)
+
+r2=LinearRegression()
+r2.fit(x_train,y_train)
+
+y_pred=r2.predict(x_test)
 
 
+#Backwards elimation
+import statsmodels.api as sm
+X=np.append(arr=np.ones((22,1)).astype(int),values=veri,axis=1)
 
+X_l=veri.iloc[:,[0,1,2,3,4,5]].values
+X_l=np.array(X_l,dtype=float)
+model=sm.OLS(boy,X_l).fit() #bağımlı değişken boy, bağımsız değ. X_l şimdi
+print(model.summary())
+## sonuçlardan çıkan en yüksek p değeri olan x5 yani bizdeki 4. elenecek
 
+X_l=veri.iloc[:,[0,1,2,3,5]].values
+X_l=np.array(X_l,dtype=float)
+model=sm.OLS(boy,X_l).fit() 
+print(model.summary())
+## bunda da x5 p değeri t den büyük yani bizdeki 5. değer, şimdi de onu eliyoruz.
 
-
-
-
-
-
+X_l=veri.iloc[:,[0,1,2,3]].values
+X_l=np.array(X_l,dtype=float)
+model=sm.OLS(boy,X_l).fit() 
+print(model.summary())
+## p değeri t den büyük olan yok.
 
 
 
