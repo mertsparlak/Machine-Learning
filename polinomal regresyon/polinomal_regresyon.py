57375abc84db41e576dde8e34f03f8d07fa9a3a0
mertsparlak <mertsparlak@gmail.com>
2024-07-19 00:17:36 +0300

    Update polinomal_regresyon.py

--- polinomal regresyon/polinomal_regresyon.py
+++ polinomal regresyon/polinomal_regresyon.py
@@ -12,46 +12,60 @@ import pandas as pd
 
 #2.veri onisleme
 #2.1.veri yukleme
-veriler = pd.read_csv('veriler.csv')
+veriler = pd.read_csv(r"C:\Users\merts\.spyder-py3\makine-ogrenmesi\polinomal regresyon\maaslar.csv")
 #pd.read_csv("veriler.csv")
 #test
 print(veriler)
 
-x = veriler.iloc[:,1:4].values #bağımsız değişkenler
-y = veriler.iloc[:,4:].values #bağımlı değişken
-print(y)
+#data frame dilimleme
+x=veriler.iloc[:,1:2] #egitim seviyesini aldk
+y=veriler.iloc[:,2:] # maaşı aldık
+X=x.values #numpy array e dönüştürdük
+Y=y.values
 
-#verilerin egitim ve test icin bolunmesi
-from sklearn.model_selection import train_test_split
+#Modeller
 
-x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=0)
-
-#verilerin olceklenmesi
-from sklearn.preprocessing import StandardScaler
-
-sc=StandardScaler()
-
-X_train = sc.fit_transform(x_train)
-X_test = sc.transform(x_test)
+#Linear regression yaptık burada sadece
+from sklearn.linear_model import LinearRegression
+lin_reg=LinearRegression()
+lin_reg.fit(X,Y)
 
 
-from sklearn.linear_model import LogisticRegression
-logr = LogisticRegression(random_state=0)
-logr.fit(X_train,y_train)
+#polinomal regresyon oluşturma
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg=PolynomialFeatures(degree=2) #2. dereceden oluşturduk
 
-y_pred = logr.predict(X_test)
-print(y_pred)
-print(y_test)
+#bu yolla direkt poly haline dönüştürebiliyoruz.
+x_poly=poly_reg.fit_transform(X)
+print(x_poly)
+
+#yine lineer reg gibi fit ediyoruz zaten olay x in exponantial olmasında artık
+lin_reg2=LinearRegression()
+lin_reg2.fit(x_poly, y)
+
+# derecesi farklı polinomal regresyon
+poly_reg=PolynomialFeatures(degree=4) #4. dereceden oluşturduk
+
+#bu yolla direkt poly haline dönüştürebiliyoruz.
+x_poly=poly_reg.fit_transform(X)
+print(x_poly)
+
+#yine lineer reg gibi fit ediyoruz zaten olay x in exponantial olmasında artık
+lin_reg2=LinearRegression()
+lin_reg2.fit(x_poly, y)
 
 
+# görselleştirmeler
+plt.scatter(X,Y,color="red")
+plt.plot(x,lin_reg.predict(X)) # doğrusal regresyon grafiği
 
+plt.scatter(X,Y,color="red")
+plt.plot(x, lin_reg2.predict(poly_reg.fit_transform(X)))
+plt.show() #2. derece grafiği
 
-
-
-
-
-
-
+plt.scatter(X,Y,color="red")
+plt.plot(x, lin_reg2.predict(poly_reg.fit_transform(X)))
+plt.show() #4. derece daha başarılı grafikten görüldüğü gibi
 
 
 
